e810639f27915ff348e76b246ec3f346d4173b19
Jann Emken <qgj.devel@gmail.com>
2023-11-24 19:27:12 +0100

    add Colors.axaml

--- GasPrices/Views/CustomControls/StationControl.axaml.cs
+++ GasPrices/Views/CustomControls/StationControl.axaml.cs
@@ -1,11 +1,19 @@
+using System.ComponentModel;
 using Avalonia;
+using Avalonia.Controls;
 using Avalonia.Controls.Primitives;
+using Avalonia.Input;
+using Avalonia.Media;
+using BruTile.Wms;
 using GasPrices.Models;
 
 namespace GasPrices.Views.CustomControls;
 
 public class StationControl : TemplatedControl
 {
+    private Border? _stationBorder;
+    private Border? _stationDistanceBorder;
+
     public static readonly StyledProperty<DisplayStation> StationProperty =
         AvaloniaProperty.Register<StationControl, DisplayStation>(nameof(Station));
 
